3694c16c0bd24500de6ee23f9c761c8edecdbc88
unknown <komodocr7@gmail.com>
2019-09-15 01:47:07 -0500

    add restriction to unauthenticated user

--- app/controllers/calories_controller.rb
+++ app/controllers/calories_controller.rb
@@ -1,5 +1,6 @@
 class CaloriesController < ApplicationController
   before_action :set_calory, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /calories
   # GET /calories.json
