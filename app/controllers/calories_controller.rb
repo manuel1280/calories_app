f5cc1cce6da7f21d596e8569b4fc3ff521cc880e
manuel1280 <komodocr7@gmail.com>
2019-09-16 10:54:44 -0500

    create callback to update registers number of user

--- app/controllers/calories_controller.rb
+++ app/controllers/calories_controller.rb
@@ -1,6 +1,7 @@
 class CaloriesController < ApplicationController
   before_action :set_calory, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  after_action :update_reister_numbers_to_user, only: [:create, :destroy]
 
   # GET /calories
   # GET /calories.json
@@ -72,5 +73,10 @@ class CaloriesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def calory_params
       params.require(:calory).permit(:value, :type_value, :comment, :user_id)
+    end
+
+    def update_reister_numbers_to_user
+      new_number = current_user.calories.pluck(:id).size
+      User.find(current_user.id).update(num_register: new_number)
     end
 end
