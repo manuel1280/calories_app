67811d50ba9b7dd58ae30b66269c474c8d4051b4
unknown <komodocr7@gmail.com>
2019-09-18 12:45:38 -0500

    refactoring send mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
-
+  config.action_mailer.default_url_options = {: host => 'https://caloric-progress.herokuapp.com'} 
   # Code is not reloaded between requests.
   config.cache_classes = true
 
