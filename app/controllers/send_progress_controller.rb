0fdfc9b9962a07fd3d50cbd6a21d58a39dd4b3d1
unknown <komodocr7@gmail.com>
2019-10-04 11:35:15 -0500

    create send porgress mailer

--- app/controllers/send_progress_controller.rb
+++ app/controllers/send_progress_controller.rb
@@ -3,6 +3,8 @@ class SendProgressController < ApplicationController
     end
 
     def send_email
-        
+        somebody = params[:email]
+        ShareMyProgressMailer.send_to_some_email(somebody).deliver_now
+        redirect_to(calories_path, notice: 'Graphic shared')
     end
 end
--- app/mailers/share_my_progress_mailer.rb
+++ app/mailers/share_my_progress_mailer.rb
@@ -1,11 +1,10 @@
 class ShareMyProgressMailer < ApplicationMailer
-    default :from => 'sanitec@example.com'
+    default from: 'sanitec@example.com'
 
     # send a signup email to the user, pass in the user object that   contains the user's email address
-    def send_to_some_email(user)
-      @user = user
-      mail( :to => @user.email,
-      :subject => 'see my caloric progress' )
+    def send_to_some_email(email)
+      byebug
+      mail( to: email, subject: 'see my caloric progress' )
     end
-  end
+
 end
